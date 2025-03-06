61412a1ef1d281cec47b654feb778ffa647965cb
btufts <btufts@andrew.cmu.edu>
2025-03-06 15:47:10 +0000

    Small bug fixes

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import json
 import utils.common as common
-from utils.config import TEMP, TOP_P
+from utils.config import TEMP, TOP_P, TOP_K, MIN_LENGTH
 from utils.language_models import load_indiv_model
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -26,31 +26,40 @@ def run(args):
         language = "French"
 
     # Set formatting request / system prompt
+    seed_text = ""
     if args.task == "code":
         formatting_request = "You are a helpful code assistant that can teach a junior developer how to code. Your language of choice is Python. Don't explain the code, just generate the code block itself."
-        post_text = "Provide code to solve the above problem: "
+        prompt_text = "Problem Prompt: "
+        seed_text = f"Here is the code to solve the problem:"
     elif "qa" in args.task:
         formatting_request = f"You are a helfpul question answering assistant that will answer a single quesetion as completely as possible given the information in the question. Do NOT using any markdown, bullet, or numbered list formatting. The assistant will use ONLY paragraph formatting. **Respond only in {language}**\nResponse:"
-        post_text = f"**Respond ONLY in {language} and no other languages**.\nResponse: "
+        prompt_text = "Question: "
+        seed_text = f"Here is the answer in {language}:"
     elif "summ" in args.task:
         formatting_request = f"You are a helfpul summarization assistant that will summarize a given article. Provide only the summarization in paragraph formatting. Do not introduce the summary. **Respond in {language}**"
-        post_text = f"**Respond ONLY in {language} and no other languages**.\nSummarization: "
+        prompt_text = "Article Content: "
+        seed_text = f"Here is the summarization in {language}:"
     elif "translation" in args.task:
         formatting_request = "You are a helpful translation assistant that will translate a given text into English. Provide only the translation and nothing else."
-        post_text = "Provide your translation of the above text: "
+        prompt_text = "Text Content: "
+        seed_text = f"Here is the translation in English:"
     elif "dialogue" in args.task:
         formatting_request = f"You are a helpful dialogue generation assistant that will generate a dialogue between people given a short paragraph describing the people involved. Provide only the dialogue. Do not introduce the dialogue. **Respond in {language}**"
-        post_text = f"**Respond ONLY in {language} and no other languages**.\nDialogue: "
+        prompt_text = "Paragraph Content: "
+        seed_text = f"Here is the dialogue in {language}:"
     elif args.task == "reviews":
         formatting_request = "You are a helpful conference paper review assistant. Please provide a detailed review of the following paper, including its strengths, weaknesses, and suggestions for improvement."
-        post_text = "Please provide a detailed review of the above paper, including its strengths, weaknesses, and suggestions for improvement.\n Review: "
+        prompt_text = "Paper Content: "
+        seed_text = "Here is the detailed review:"
     elif args.task == "abstract":
         formatting_request = "You are a helpful abstract writing assistant. You will write an abstract given the content of a paper. Do not provide any other text. You will only provide an abstract."
-        post_text = "Provide an abstract of the above paper: "
+        prompt_text = "Paper Content: "
+        seed_text = "Here is the detailed abstract:"
     
     if args.prompt == "rewrite":
         formatting_request = "You are a helpful writing assistant. Rewrite the following text to improve clarity and professionalism. Do not provide any other text. Only provide the rewritten text."
-        post_text = f"Provide the rewritten text in {language}: "
+        prompt_text = "Text to rewrite: "
+        seed_text = f"Here is the rewritten text in {language}:"
 
     # Load data
     df = pd.read_csv(args.data)
@@ -66,66 +75,6 @@ def run(args):
         & (df["task"] == args.task)
     ]
 
-    # Replace with the exact model path if needed
-    # model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
-    # likert_tokenizer = AutoTokenizer.from_pretrained(
-    #     model_name, token=os.getenv("HUGGING_FACE_TOKEN")
-    # )
-    # likert_model = AutoModelForCausalLM.from_pretrained(
-    #     model_name,
-    #     device_map="auto",
-    #     torch_dtype=torch.float16,
-    #     attn_implementation="flash_attention_2",
-    #     token=os.getenv("HUGGING_FACE_TOKEN"),
-    # )
-
-    # data = []
-    # indexs = []
-    # likert_fails = 0
-    # if args.end < 0:
-    #     args.end = len(original_data)
-
-    # if args.template:
-    #     ref_key = f"{args.model}_template_answer"
-    # elif args.task == "rewrite":
-    #     ref_key = f"{args.model}_rewrite_answer"
-    # else:
-    #     ref_key = f"{args.model}_base_answer"
-
-    # for i in tqdm(
-    #     range(len(original_data[args.start : args.end])),
-    #     desc="Evaluating Previous Output",
-    # ):
-    #     if ref_key in original_data[i] and not args.regen:
-    #         length = len(
-    #             likert_tokenizer(original_data[i][ref_key])["input_ids"]
-    #         )
-    #         likert_score = -1
-    #         retries = 5
-    #         while likert_score < 0 and retries > 0:
-    #             likert_score = common.generate_likert_scores(
-    #                 original_data[i][prompt_text],
-    #                 original_data[i][ref_key],
-    #                 likert_tokenizer,
-    #                 likert_model,
-    #             )
-    #             retries -= 1
-    #         if length < 100 or (likert_score < 3 and likert_score > 0):
-    #             data.append(original_data[i])
-    #             indexs.append(i)
-    #         if likert_score < 0:
-    #             likert_fails += 1
-    #     else:
-    #         data.append(original_data[i])
-    #         indexs.append(i)
-
-    # print("LIKERT FAILS: ", likert_fails)
-    # print("TOTAL RERUNS: ", len(data))
-
-    # del likert_tokenizer
-    # del likert_model
-
-    # load models and template names
     model = load_indiv_model(args.model)
 
     full_prompts = []
@@ -134,7 +83,7 @@ def run(args):
             # no template
             chat = [
                 {"role": "system", "content": formatting_request},
-                {"role": "user", "content": row.text},
+                {"role": "user", "content": prompt_text + row.text},
             ]
         elif args.prompt == "template":
             # yes template
@@ -144,11 +93,9 @@ def run(args):
                     "content": formatting_request
                     + "Try to sound as human as possible.",
                 },
-                {"role": "user", "content": row.text},
+                {"role": "user", "content": str(row.text)},
             ]
         full_prompts.append(chat)
-
-    len(full_prompts)
 
     # Get all of the outputs
     outputs_list = []
@@ -160,8 +107,10 @@ def run(args):
                 max_n_tokens=args.max_n_tokens,
                 max_input_tokens=args.max_input_tokens,
                 temperature=TEMP,
-                end_text=post_text,
-                top_p=TOP_P
+                seed_text=seed_text,
+                top_p=TOP_P,
+                top_k=TOP_K,
+                min_length = MIN_LENGTH
             )
         )
 
@@ -180,11 +129,23 @@ def run(args):
 
     key_columns = ["task", "text_id", "prompt", "model"]
     new_df = pd.DataFrame(new_rows)
-    merged_df = pd.concat([new_df, df]).drop_duplicates(
-        subset=key_columns, keep="first"
-    )
-    output_file = args.output_file if args.output_file else args.data
-    merged_df.to_csv(output_file, index=False)
+    if args.output_file:
+        new_df = pd.concat([new_df, data]).drop_duplicates(
+            subset=key_columns, keep="first"
+        )
+        if os.path.exists(args.output_file):
+            existing_df = pd.read_csv(args.output_file)
+            merged_df = pd.concat([new_df, existing_df]).drop_duplicates(
+                subset=key_columns, keep="first"
+            )
+        else:
+            merged_df = new_df
+        merged_df.to_csv(args.output_file, index=False)
+    else:
+        merged_df = pd.concat([new_df, df]).drop_duplicates(
+            subset=key_columns, keep="first"
+        )
+        merged_df.to_csv(args.data, index=False)
 
 
 if __name__ == "__main__":
--- run_experiments.py
+++ run_experiments.py
@@ -51,46 +51,47 @@ def run_detect(config, all_judge_configs):
 
 def run_generate(config):
     data = config["data"]
-    task = config["task"]
+    tasks = config["tasks"]
     prompt = config["prompt"]
     model = config["model"]
     device = config.get("device", None)
-    max_n_tokens = config.get("max_n_tokens", None)
-    max_input_tokens = config.get("max_input_tokens", None)
+    max_n_tokens = str(config.get("max_n_tokens", None))
+    max_input_tokens = str(config.get("max_input_tokens", None))
     output_file = config.get("output_file", None)
-    start = config.get("start", None)
-    end = config.get("end", None)
+    start = str(config.get("start", None))
+    end = str(config.get("end", None))
     language = config.get("language", None)
 
-    cmd = [
-        "python",
-        "generate.py",
-        "--data",
-        data,
-        "--task",
-        task,
-        "--model",
-        model,
-        "--prompt",
-        prompt,
-    ]
-    if device:
-        cmd.extend(["--device", device])
-    if max_n_tokens:
-        cmd.extend(["--max-n-tokens", max_n_tokens])
-    if max_input_tokens:
-        cmd.extend(["--max-input-tokens", max_input_tokens])
-    if output_file:
-        cmd.extend(["--output-file", output_file])
-    if start:
-        cmd.extend(["--start", start])
-    if end:
-        cmd.extend(["--end", end])
-    if start:
-        cmd.extend(["--language", language])
+    for task in tasks:
+        cmd = [
+            "python",
+            "generate.py",
+            "--data",
+            data,
+            "--task",
+            task,
+            "--model",
+            model,
+            "--prompt",
+            prompt,
+        ]
+        if device:
+            cmd.extend(["--device", device])
+        if max_n_tokens:
+            cmd.extend(["--max-n-tokens", max_n_tokens])
+        if max_input_tokens:
+            cmd.extend(["--max-input-tokens", max_input_tokens])
+        if output_file:
+            cmd.extend(["--output-file", output_file])
+        if start:
+            cmd.extend(["--start", start])
+        if end:
+            cmd.extend(["--end", end])
+        if start:
+            cmd.extend(["--language", language])
 
-    print(f'Executing: {" ".join([str(item) for item in cmd])}')
-    subprocess.run(cmd)
+        print(f'Executing: {" ".join([str(item) for item in cmd])}')
+        subprocess.run(cmd)
 
     return True
 
